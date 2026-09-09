4502955e0813c5cf9a6608f2c01f0cfa545e5f74
RAFAELA RAMOS <rafaela_ramos151@estudante.sesisenai.org.br>
2026-09-08 22:17:29 -0300

    adicao exemplo00.js

---
+++ exemplo00.js
@@ -0,0 +1,1 @@
+console.log ("PALMEIRAS NÃO TEM MUNDIAL");
---
+++ exemplo001.js
@@ -0,0 +1,7 @@
+if (true) {
+    let local = 'visível apenas aqui';
+    var global = 'vaza para fora';
+    console.log(global); // erro
+  }
+  
+ // console.log(global); // Funciona
